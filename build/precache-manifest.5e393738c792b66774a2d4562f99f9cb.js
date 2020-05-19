37b1ede156dba65eaec6d6ff036928c1b5e93146
Muhiddin Kurbonov <muhiddin.m.kurbonov@gmail.com>
2020-05-18 22:22:53 -0700

    sc

---
+++ build/precache-manifest.5e393738c792b66774a2d4562f99f9cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74a32a6cc306dffed72135f7a7811559",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a8f228273d7433d3bc2",
+    "url": "/static/css/main.8cb498fc.chunk.css"
+  },
+  {
+    "revision": "c87fdb5966d2750f572a",
+    "url": "/static/js/2.04e9a8a2.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.04e9a8a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a8f228273d7433d3bc2",
+    "url": "/static/js/main.33693840.chunk.js"
+  },
+  {
+    "revision": "72c5965cbfb3ac6a55d7",
+    "url": "/static/js/runtime-main.40d8ffab.js"
+  },
+  {
+    "revision": "862646c4c51c3afffbf5aa15ca0d8f6a",
+    "url": "/static/media/bg.862646c4.jpg"
+  }
+]);
